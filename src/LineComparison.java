3eb6b2f5f0ee857ef16f2f451c77e3887612e49e
jasmine217k <jasmine217k@gmail.com>
2023-11-23 11:38:31 +0530

    Changing code to use concept of Objects

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,44 +1,86 @@
 import java.util.Scanner;
 
+
 public class LineComparison {
+    public static class Line implements Comparable<Line>
+    {
+        //Coordinates of Point A
+       private double x1;
+       private double y1;
+
+       //Coordinates of Point B
+        private double x2;
+        private double y2;
+
+        public double calculateLength()
+        {
+            return Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
+        }
+
+        public void printLength(double len)
+        {
+            System.out.println("The length of the line is "+len);
+        }
+        public Line(double x1,double y1,double x2,double y2)
+        {
+            this.x1=x1;
+            this.x2=x2;
+            this.y1=y1;
+            this.y2=y2;
+        }
+
+
+        @Override
+        public int compareTo(Line l) {
+
+            double l1=this.calculateLength();
+            double l2=l.calculateLength();
+
+            return Double.compare(l1,l2);
+        }
+    }
     public static void main(String[] args) {
 
         System.out.println("Welcome to Line Comparison Problem");
         Scanner sc=new Scanner(System.in);
 
-        //Take 1st coordinate (x1,y1) as input
+        //Taking 1st coordinate (x1,y1) as input
+        System.out.println("Enter the coordinates (x1,y1) of 1st line");
+        double x1=sc.nextInt();
+        double y1=sc.nextInt();
 
-        //Taking 1st coordinate as input
-        System.out.println("Enter the coordinates (x1,y1) of the line");
-        int x1=sc.nextInt();
-        int y1=sc.nextInt();
+        //Taking 2nd coordinate (x2,y2) as input
+        System.out.println("Enter the coordinates (x2,y2) of 1st line");
+        double x2=sc.nextInt();
+        double y2=sc.nextInt();
 
-        //Taking 2nd coordinate as input
-        System.out.println("Enter the coordinates (x2,y2) of the line");
-        int x2=sc.nextInt();
-        int y2=sc.nextInt();
+        //Forming object of class Line
+       Line l1=new Line(x1,y1,x2,y2);
 
-        //Calculating and printing the length of first line
-        Double length1=Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
-
-        System.out.println("The length of the first line : "+length1);
+       //Calculating and printing length of Line 1
+       double len1= l1.calculateLength();
+       l1.printLength(len1);
 
         System.out.println("Enter the coordinates of (x3,y3) of 2nd line");
         int x3=sc.nextInt();
         int y3=sc.nextInt();
 
 
-        System.out.println("Enter the coordinates (x4,y4) of the line");
+        System.out.println("Enter the coordinates (x4,y4) of the 2nd line");
         int x4=sc.nextInt();
         int y4=sc.nextInt();
 
-        //Calculating and printing the length of second line
-        Double length2=Math.sqrt(Math.pow(x3-x4,2)+Math.pow(y3-y4,2));
+        //Calculating and printing length of Line 2
+        Line l2=new Line(x3,y3,x4,y4);
 
-        System.out.println("The length of the second line : "+length2);
+        //Calculating and printing the length of second line
+       double len2=l2.calculateLength();
+       l2.printLength(len2);
+
+
 
         //Comparing both the lengths
-        int result = length1.compareTo(length2);
+        int result = l1.compareTo(l2);
 
         if (result < 0) {
             System.out.println("Line1 is smaller in length than Line2");
